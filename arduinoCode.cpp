9b0e84449175ed3f83cb903dfc2a362ce470bced
Frederic Branch <161014378+fredericomuchacho@users.noreply.github.com>
2024-02-23 10:59:25 +0200

    Create arduinoCode.cpp

---
+++ arduinoCode.cpp
@@ -0,0 +1,26 @@
+ if(_length != length || _value == nullptr){
+    _length = length;
+    if( _value != nullptr){
+      delete[] _value;
+    }
+    _value  = new char[_length + 1];  
+  }
+
+
+  if(p->getID()){
+    for (size_t i = 0; i < strlen(p->getID()); i++){
+       if(!(isAlphaNumeric(p->getID()[i])) && !(p->getID()[i]=='_')){
+        #ifdef WM_DEBUG_LEVEL
+        DEBUG_WM(WM_DEBUG_ERROR,F("[ERROR] parameter IDs can only contain alpha numeric chars"));
+        #endif
+        return false;
+       }
+    }
+  }
+
+  if(_params == NULL){
+    #ifdef WM_DEBUG_LEVEL
+    DEBUG_WM(WM_DEBUG_DEV,F("allocating params bytes:"),_max_params * sizeof(WiFiManagerParameter*));        
+    #endif
+    _params = (WiFiManagerParameter**)malloc(_max_params * sizeof(WiFiManagerParameter*));
+  }
